78f6f448d37bbe1d508edfbe59c477ccaa1c7d72
nightWolf666 <chsturm@gmx.de>
2024-05-06 17:49:22 +0200

    Revert "Pages_for_dev_update"
    This reverts commit 2952d10cc5764a3eea6e3bbb45919ca217d19deb.

--- src/pages/Event_Create.jsx
+++ src/pages/Event_Create.jsx
@@ -3,7 +3,7 @@ import { useBackgroundImage } from "../context/BackgroundImageContext.jsx";
 import { useFetch } from "../hooks/useFetch.js";
 import Background from '../assets/background/Background_Strand.png';
 import Button from "../ui/Button.jsx";
-import styles from "../assets/css/event_create.module.css";
+import styles from "../assets/css/login.module.css";
 import Stage from "../assets/elements/Bühne_final.png";
 
 
@@ -11,39 +11,27 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 const Event_Create = () => {
   const { setBackgroundImage } = useBackgroundImage();
-  const [user, setUser] = useState([]);
+  const [event, setEvent] = useState([]);
   const [event_name, setEvent_name] = useState("");
   const [event_ort, setEvent_ort] = useState("");
   const [event_beschreibung_kurz, setEvent_beschreibung_kurz] = useState("");
+  const [event_start, setEventstart] = useState();
+  const [event_ende, setEventende] = useState();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [event_genre, setEvent_genre] = useState("");
-  
-  
-  useEffect(() => {
-    fetch(import.meta.env.VITE_SERVER_URL + '/user/2')
-      .then(response => response.json())
-      .then(data => setUser(data[0]))
-      .catch(error => console.error('Error:', error));
-  }, []);
+  const [event_genre, setEvent_genre] = useState("");  
 
- 
-
-  // const [error, backendFetchResult] = useFetch('https://rocknchill-backend-new-1.onrender.com/user/1');
+  const [error, backendFetchResult] = useFetch('https://pokeapi-be-5p2b.onrender.com/event');
 
   useEffect(() => {
     setBackgroundImage(Background);
   }, []);
 
-  // useEffect(() => {
-  //   if (backendFetchResult?.length > 0) {
-  //     setUser(backendFetchResult);
-      
-  //   }
-  //   console.log("inside", backendFetchResult);
-  // }, [backendFetchResult]);
-  // const [user_var] = user; 
-  // console.log("outside w User",user_var.user_id);
+  useEffect(() => {
+    if (backendFetchResult?.length > 0) {
+      setEvent(backendFetchResult);
+    }
+  }, [backendFetchResult]);
 
   // const handleEvent = () => {
   //   const eventData = {
@@ -55,56 +43,48 @@ const Event_Create = () => {
   //     genre
   //   };
 
-    const handleEvent = (e) => {
-      e.preventDefault();
-      console.log(user);
-      
-      
+    const handleEvent = () => {
+      setEventstart(startDate);
+      setEventende(endDate);
 
-      const user_id = user.user_id;
       const eventData = {
         event_name,
         event_ort,
-        event_start:startDate,
-        event_ende:endDate,
+        event_start,
+        event_ende,
         event_beschreibung_kurz,
-        event_genre,
-        user_id
+        event_genre
       };
 
-      
-        fetch(import.meta.env.VITE_SERVER_URL + '/event', {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(eventData)
-        })
-        .then(response => response.json())
-        .then(data => {
-
-          console.log('Server response:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-      
-      };
+    fetch('https://pokeapi-be-5p2b.onrender.com/event', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(eventData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Server response:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  };
 
   return (
     <>
     
-    <div className={styles.event_create}>
+    <div className={styles.login}>
     {/* <span><img  className={styles.container} src={Stage} alt="Stage" /></span> */}
             <div className={styles.blur}></div>
             <div className={styles.container}>
-              <form onSubmit={handleEvent} className={styles.wrapper}>
+              <div className={styles.wrapper}>
                 <div className={styles.header}>
                   <div className={styles.line}></div>
                   <span className={styles.h1}>Nächste Veranstaltung</span>
                   <div>
                     <span className={styles.h2}> Bitte trag dein Event ein
-                    
                     <input type="text" className="styledinput" placeholder="Veranstaltung" value={event_name} onChange={(e) => setEvent_name(e.target.value)} />
                     <br />
                     <input type="text" className="styledinput" placeholder="Ort" value={event_ort} onChange={(e) => setEvent_ort(e.target.value)} />
@@ -114,8 +94,6 @@ const Event_Create = () => {
                     <input type="text" className="styledinput" placeholder="Genre" value={event_genre} onChange={(e) => setEvent_genre(e.target.value)} />
                     
                     
-
-
                   </span>
                   </div>
                   <DatePicker
@@ -134,9 +112,9 @@ const Event_Create = () => {
                     minDate={startDate}
                   />
                   <br />
-                  <button className={styles.styledbutton} >Eintragen</button>
+                    <button onClick={handleEvent}>Eintragen</button>
                 </div>
-              </form>
+              </div>
             </div>
         </div>
         
--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,41 +1,35 @@
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useBackgroundImage } from "../context/BackgroundImageContext.jsx";
+import { useFetch } from "../hooks/useFetch.js";
+import Button from "../ui/Button.jsx";
+import HomeButton from '../assets/icons/Logo.png';
 import Background from '../assets/background/Background_Strand.png';
-import "../pages/Profil.css";
+
+
 
 const Profil = () => {
+
   const { setBackgroundImage } = useBackgroundImage();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState([]);
+
+  const [error, backendFetchResult] = useFetch(import.meta.env.VITE_SERVER_URL + "/user");
 
   useEffect(() => {
     setBackgroundImage(Background);
   }, []);
 
   useEffect(() => {
-    fetch('https://rocknchill-backend-new-1.onrender.com/user/1')
-      .then(response => response.json())
-      .then(data => setUser(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
+    if (backendFetchResult?.length > 0) {
+      setUser(backendFetchResult);
+    }
+  }, [backendFetchResult]);
 
   return (
-    <div className="profil-container">
-      <h1>RocknChill</h1>
-      <p>Benutzername: {user.user_name}</p>
-      <p>Email: {user.email}</p>
-      <p>Urlaubstage: {user.urlaubstage}</p>
-      <p>Budget: {user.budget}</p>
-      <p>Avatar Link: {user.avatar_link}</p>
+    <div>
+      <Button className="home-button" img={HomeButton} url="/dashboard"/>
     </div>
-  );
-};
+  )
+}
 
 export default Profil;
-
-
-
-
