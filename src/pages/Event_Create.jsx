950aed75195b1af9e6a08cc279d02306e631370a
nightWolf666 <chsturm@gmx.de>
2024-05-08 10:14:18 +0200

    Button Komponete nimmt jetzt onClick Funktion an

--- src/pages/Event_Create.jsx
+++ src/pages/Event_Create.jsx
@@ -55,8 +55,10 @@ const Event_Create = () => {
   //     genre
   //   };
 
-    const handleEvent = () => {
-      
+  const e = null;
+
+    const handleEvent = (e) => {
+      // e.preventDefault();
       console.log(user);
       
       
@@ -134,7 +136,7 @@ const Event_Create = () => {
                     minDate={startDate}
                   />
                   <br />
-                  <Button handleEvent={handleEvent} text="Eintragen" url="/dashboard"/>
+                  <Button type="submit" handleEvent={handleEvent} text="Eintragen" url="/dashboard"/>
                 </div>
               </form>
             </div>
